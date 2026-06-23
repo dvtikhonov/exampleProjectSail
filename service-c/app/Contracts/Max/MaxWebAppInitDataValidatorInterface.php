55ebc552d424cf6f3db6f7c8e769eef2b1380077
dvtikhonov <dv_tikhonovas.ru@mail.ru>
2026-06-23 13:49:07 +0700

    service-c комменты #2

--- service-c/app/Contracts/Max/MaxWebAppInitDataValidatorInterface.php
+++ service-c/app/Contracts/Max/MaxWebAppInitDataValidatorInterface.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Contracts\Max;
 
 use App\DTO\Max\MaxWebAppInitDataDto;
+use App\Exceptions\Max\MaxWebAppInitDataException;
 
 /**
  * Валидация и разбор initData MAX WebApp.
@@ -14,7 +15,7 @@ interface MaxWebAppInitDataValidatorInterface
     /**
      * Проверяет подпись и срок действия initData.
      *
-     * @throws \App\Exceptions\Max\MaxWebAppInitDataException
+     * @throws MaxWebAppInitDataException
      */
     public function validate(string $initData): MaxWebAppInitDataDto;
 }
